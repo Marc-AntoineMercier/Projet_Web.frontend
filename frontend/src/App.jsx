ba5f4b97e8fafeaa978a8cf6c32a9d0dc53d013e
marc-antoine mercier <marc.mercier.mam@gmail.com>
2025-02-01 18:17:13 -0500

    Faire le visuel du dashboard avec le changement de theme (light/dark). Aucune Route n'est encore fonctionnel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./navbar/TopBar";
+import SideBar from "./navbar/SideBar"
 
-function App() {
-
+export default function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SideBar />
+          <div className="content">
+            <TopBar/>
+            <Routes>
+              
+            </Routes>
+          </div>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+    </BrowserRouter>
   )
 }
-
-export default App
