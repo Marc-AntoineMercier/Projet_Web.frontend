e0223411cc16ac3531cceeb872b4b32c09f63ff4
marc-antoine mercier <marc.mercier.mam@gmail.com>
2025-02-07 19:39:02 -0500

    ajout de la fonctionnalite du footer, mais il reste a mettre les information dedans pour qu'il soit terminer

--- src/navbar/Footer.jsx
+++ src/navbar/Footer.jsx
@@ -1,10 +1,26 @@
+import { AppBar, Box } from "@mui/material"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from "react-router-dom"
+
+const Item = ({title, to, icon}) => {
+    <Box>
+        <Box>{icon}</Box>
+        
+        <Link to={to}>{title}</Link>
+    </Box>
+}
+
 
 function Footer(){
 
     return (
-        <div style={{display:"flex", alignItems:"center"}}>
-            <p>Footer</p>
-        </div>
+        <AppBar position="static" sx={{bottom:0, width:"100%", position:"fixed", p:2}}>
+            <Box textAlign={"center"} >
+                <GitHubIcon sx={{mr:2}}/>
+                <Link to={"https://github.com/Marc-AntoineMercier/Projet_Web.frontend"}>FrontEnd</Link>
+            </Box>
+            
+        </AppBar>
     )
 }
 
